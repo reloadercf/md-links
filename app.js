0b867266b9df52aae279ddbc1676295f36deae33
reloadercf <elcfm1@gmail.com>
2020-05-08 06:27:49 -0500

    v 1.0 end

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 let { extraer } = require('./lib/extractor.js')
 let { consulterUrl } = require('./lib/consulter')
 
-let validateLink = async() => {
+let validateLink = async(pwd) => {
 
-    let extract = await extraer()
+    let extract = await extraer(pwd)
     let consulter = await extract.forEach(urls => {
         consulterUrl(urls)
     })
     return `${consulter}`
 }
-validateLink().then(mensaje => {
-    console.log(mensaje)
-}).catch(e => {
-    console.log(e)
-})
+let statsLink = async(pwd) => {
+    let extract = await extraer(pwd)
+    let all = await extract
+    return `${all.length}`
+}
+
+module.exports = {
+    validateLink,
+    statsLink
+}
